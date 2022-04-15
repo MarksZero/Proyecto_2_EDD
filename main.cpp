190d16f2b04c185e3310cfdd748eb22a4dab00e7
pablo <p.martinez10@ufromail.cl>
2022-04-15 00:42:47 -0400

    b

--- main.cpp
+++ main.cpp
@@ -15,32 +15,37 @@ void saludo();
 
 
 int main() {
+    FILE * arch = fopen ("Listas.txt", "r");
     saludo();
     int opcion;
     cin>>opcion;
 
     int numero_de_usuarios=0;
-    Usuario c[numero_de_usuarios];
+    Usuario u[numero_de_usuarios];
 
     //----------------------------------------------------
 
-if(opcion==1) {
-    //CREACION DE USUARIOS
-    cout<<numero_de_usuarios <<"\n";
-    numero_de_usuarios++;
-    for (int i = 0; i < numero_de_usuarios; i++) {
-        ingresarUsuario(&c[i]);
-        printf("%s\n", c[i].nombre);
+    if(opcion==1) {
+        //CREACION DE USUARIOS
+        cout<<numero_de_usuarios <<"\n";
+        numero_de_usuarios++;
+        for (int i = 0; i < numero_de_usuarios; i++) {
+            ingresarUsuario(&u[i]);
+            printf("%s\n", u[i].nombre);
+            fscanf(stdin, "%c", &u[i].nombre);
+           // fscanf(arch, "%lu", &u[i].rut);
 
 
+        }
     }
-}
-    //----------------------------------------------------
-else if(opcion==2){
-    cout<<numero_de_usuarios <<"\n";
-    printf("%s\n", c->nombre[1]);
-    printf("%lu\n", c->rut);
-}
+        //----------------------------------------------------
+    else if(opcion==2){
+        /* cout<<numero_de_usuarios <<"\n";
+         printf("%s\n", u->nombre[1]);
+         printf("%lu\n", u->rut);
+         */
+    }
+    fclose(arch);
     return 0;
 }
 
